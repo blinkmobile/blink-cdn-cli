962caf327ba7058062578d0d873f6522192bdb16
Zac Turner <zac@oneblink.io>
2020-03-04 15:42:49 +1100

    ON-5283 # Fixed eslint issue

--- test/region-origin.js
+++ test/region-origin.js
@@ -2,7 +2,7 @@
 
 const test = require('ava')
 
-const tenant = require('../lib/tenant')
+const tenantData = require('../lib/tenant')
 
 test.beforeEach(t => {
   t.context.logger = {
@@ -12,18 +12,18 @@ test.beforeEach(t => {
 
 test('Should get correct origin for ONEBLINK tenant', t => {
   const tenant = 'oneblink'
-  const origin = tenant.getOrigin(tenant)
-  t.is(origin, tenant.TENANTS.ONEBLINK)
+  const origin = tenantData.getOrigin(tenant)
+  t.is(origin, tenantData.TENANTS.ONEBLINK)
 })
 
 test('Should get correct origin for CIVICPLUS tenant', t => {
   const tenant = 'civicplus'
-  const origin = tenant.getOrigin(tenant)
-  t.is(origin, tenant.TENANTS.CIVICPLUS)
+  const origin = tenantData.getOrigin(tenant)
+  t.is(origin, tenantData.TENANTS.CIVICPLUS)
 })
 
 test('Should get correct origin for NO tenant', t => {
   const tenant = null
-  const origin = tenant.getOrigin(tenant)
-  t.is(origin, tenant.TENANTS.ONEBLINK)
+  const origin = tenantData.getOrigin(tenant)
+  t.is(origin, tenantData.TENANTS.ONEBLINK)
 })
